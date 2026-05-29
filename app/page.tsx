7c70b9c514d6910b0cbda3636b6c2ed0d970c61c
Aswin Krishna <your_email@example.com>
2026-05-29 22:27:58 +0530

    removed many

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,11 @@
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero"
-import Food from "./components/Food/Food"
-import Stay from "./components/Stay/Stay"
-import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer"
 export default function Home() {
   return (
     <div className="app">
       <NavBar />
       <Hero/>
-      <Food/>
-      <Stay/>
-      <Contact/>
-      <Footer/>
     </div>
   );
 }
